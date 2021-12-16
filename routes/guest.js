12145d296c87fabeb275cbe10a9535ebfa23dd17
Cpp4Life <Cpp4Life@users.github.com>
2021-12-16 23:13:00 +0700

    configure passport and middleware for authentication

--- routes/guest.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const guestController = require('../controllers/guest');
-const router = express.Router();
-
-router.get('/', guestController.getHomePage);
-
-module.exports = router;
