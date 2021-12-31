bc38d3271920a0493a702d004b080a4efb4ce682
Cpp4Life <Cpp4Life@users.github.com>
2021-12-31 15:29:56 +0700

    fix app.js and auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    if (req.isAuthenticated()) {
+    if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
         req.flash('error_msg', 'Please log in to view that resource');
         return res.redirect('/user/login');
